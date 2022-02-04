31b790361fcfa64dbfaea115c672681f34060cc8
Filipe Sales Araujo <filipesalesaraujo@gmail.com>
2022-02-04 01:33:20 -0300

    finalizado

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,38 +1,22 @@
+import { useMovies } from "../MoviesContext";
 import { MovieCard } from "./MovieCard";
 
-interface ContentProps {
-  selectedGenre: {
-    id: number;
-    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
-    title: string;
-  };
+export function Content() {
+  const {selectedGenre, movies} = useMovies()
 
-  movies: Array<{
-    imdbID: string;
-    Title: string;
-    Poster: string;
-    Ratings: Array<{
-      Source: string;
-      Value: string;
-    }>;
-    Runtime: string;
-  }>;
-}
-
-export function Content({ selectedGenre, movies }: ContentProps) {
   return (
     <div className="container">
-      <header>
-        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
-      </header>
+        <header>
+          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+        </header>
 
-      <main>
-        <div className="movies-list">
-          {movies.map(movie => (
-            <MovieCard key={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-          ))}
-        </div>
-      </main>
-    </div>
+        <main>
+          <div className="movies-list">
+            {movies.map(movie => (
+              <MovieCard key={movie.Title} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </div>
   )
 }
